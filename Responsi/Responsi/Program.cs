e8aa481f46daaddbd641a819b01b343a39dda2a9
nabillawr <nabillawr15@gmail.com>
2019-04-23 10:47:48 +0700

    Responsi

---
+++ Responsi/Responsi/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResponsiPemrog2318
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Karyawan kry1 = new Karyawan();
+
+            kry1.Nik = "18112318";
+            kry1.Nama = "Billa";
+            kry1.GajiBulanan = 3000000;
+
+            Karyawan kry2 = new Karyawan();
+
+            kry2.Nik = "18112342";
+            kry2.Nama = "Vicka";
+            kry2.GajiBulanan = 2000000;
+
+
+            Console.WriteLine("No.\tNik/Nama\t\tGaji Bulanan");
+            Console.WriteLine("-----------------------------------------------------------");
+            Console.WriteLine("1.\t{0}  {1}\t\t{2}", kry1.Nik, kry1.Nama, kry1.GajiBulanan);
+            Console.WriteLine("2.\t{0}  {1}\t\t{2}", kry2.Nik, kry2.Nama, kry2.GajiBulanan);
+
+            Console.WriteLine("\n\n");
+            Console.WriteLine("Asyiiik Kenaikan Gaji 10%");
+            Console.WriteLine("\n");
+            Console.WriteLine("No.\tNik/Nama\t\tGaji Bulanan");
+            Console.WriteLine("------------------------------------------------------------");
+            Console.WriteLine("1.\t{0}  {1}\t\t{2}", kry1.Nik, kry1.Nama, kry1.GajiBulanan + (kry1.GajiBulanan * 0.10));
+            Console.WriteLine("2.\t{0}  {1}\t\t{2}", kry2.Nik, kry2.Nama, kry2.GajiBulanan + (kry2.GajiBulanan * 0.10));
+            Console.ReadKey();
+        }
+    }
+}
